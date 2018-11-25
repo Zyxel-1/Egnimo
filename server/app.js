9d9012a3ba82cbfe65f296e33a4a832c3dd7ea0a
Roberto Sanchez <rsanchez92@live.com>
2018-11-25 12:37:52 -0800

    Base Setup
    Created a basic REST function taking in a json object and printing it out

--- server/app.js
+++ server/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-app.use(express.static('public'))
+//app.use(express.static('public'))
+app.get('/', function (req, res) {
+    res.send('Sample Get function')
+});
+app.post('/api/register', function (req, res) {
+    console.log(req.body)
+    res.send('Got a POST request')
+})
+app.post('/api/login', (res, req) => {
+    res.send('Login')
+});
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT);
 console.log(`Now listening on port: ${PORT}`)
