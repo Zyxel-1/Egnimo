a799687a74c08cc098c6a53af4af607b5193687c
Roberto Sanchez <rsanchez92@live.com>
2019-02-13 20:11:24 -0800

    Beginning Authentication

--- server/server/app.js
+++ server/server/app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/', function (req, res) {
+app.get('/', (req, res)=> {
     res.send('Sample Get function')
 });
-app.post('/api/register', function (req, res) {
+app.post('/api/register', (req, res) => {
     console.log(req.body)
-    res.send('Got a POST request')
+    res.send(`Got the following data: ${req.body.username} and ${req.body.password}`)
 })
-app.post('/api/login', (res, req) => {
-    res.send('Login')
+app.post('/api/login', (req, res) => {
+    console.log(req.body)
+    res.send('Got a Login request')
 });
 
 const PORT = process.env.PORT || 3000
