85e4b0ef5c8090493db598414aa93dd2170c9c62
Roberto Sanchez <rsanchez92@live.com>
2019-02-14 10:30:49 -0800

    Setting up for oauth

--- server/app.js
+++ server/app.js
@@ -7,12 +7,10 @@ const {constraints} = require('./validations/constraints')
 const {User} = require('./models/user');
 // Importing database
 var {mongoose} = require('./database/mongoose');
-
-
+// Setting up express
+const PORT = process.env.PORT || 3000
 const app = express();
 app.use(express.json());
-
-const PORT = process.env.PORT || 3000
 // -------------------------------------------------------------------------
 //                      Routes
 // -------------------------------------------------------------------------
@@ -20,9 +18,11 @@ const PORT = process.env.PORT || 3000
 app.get('/', (req, res)=> {
     res.send('Hello')
 });
+
+// -------------------------------------------------------------------------
 // Route(/api/register)
-// Takes in a username and a hashed password in a json object from client. Once taken
-// in it generates a token for that user.
+//      Takes in a username and a hashed password in a json object from client. Once taken
+//      in it generates a token for that user.
 app.post('/api/register', (req, res) => {
     // Extract data needed for registration
     var body = _.pick(req.body, ['username', 'password']);
@@ -34,21 +34,29 @@ app.post('/api/register', (req, res) => {
         // Store incoming data
         var user = new User(body);
         user.save().then(()=>{
-            console.log('saved')
-            res.send(`Got the following data: ${body.username} and ${body.password}`)
+            res.send('Registration Successful').status(200);
         }).catch((e)=>{
             res.send(`An error occurred while saving into DB: ${e}`).status(500);
-        })        
+            //res.send('An error occurred while processing your request').status(500)
+        });        
     }
 });
 // Route(/api/login)
 // Takes in a username and password and returns a token.
 app.post('/api/login', (req, res) => {
-    console.log(req.body)
+    // Validate incoming data
+    var failures = validate(body,constraints);
+    if(failures){
+        res.send(failures).status(400);
+    }
+    // Check Db if they exists
+    
+    // Return token
     res.send('Got a Login request')
 });
 //--------------------------------------------------------------------------
 //                      Express Setup
 //--------------------------------------------------------------------------
+
 app.listen(PORT);
 console.log(`Now listening on port: ${PORT}`)
--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Define schema
 var Schema = mongoose.Schema;
 
-var user = new Schema({
+var UserSchema = new Schema({
     username: {
         type: String,
         required: true,
@@ -28,6 +28,28 @@ var user = new Schema({
     }]
 });
 
-var User = mongoose.model('User',user);
+var User = mongoose.model('User',UserSchema);
 
+// Finds credential in Database
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+    // Returns user that is found
+    return User.findOne({email}).then((user)=>{
+        // reject promise if there isn't any matches
+        if(!user){
+            return Promise.reject();
+        }
+        // If found compare password from function to password in user found.
+        return new Promise((resolve,reject)=>{
+            // Use bcrypt.compare to compare password and user.password
+            bcrypt.compare(password,user.password,(err,res)=>{
+                if(res){
+                    resolve(user);
+                } else{
+                    reject();
+                }
+            });
+        });
+    });
+};
 module.exports = {User};
