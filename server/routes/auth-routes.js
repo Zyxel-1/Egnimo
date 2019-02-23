cf3b9422fedddbc5e8edd838fd5626bd508909fd
Roberto Sanchez <rsanchez92@live.com>
2019-02-22 21:17:01 -0800

    Cleaning up Docs branch to support documentation

--- server/routes/auth-routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.post('/api/profile', async (req,res)=>{
-    res.status(200).send('This is a protected route. You have access to it.')
-});
-
-module.exports = router;
